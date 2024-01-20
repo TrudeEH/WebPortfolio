962437de6bb109e173151fc4d7d088cdb731b41d
TrudeEH <ehtrude@gmail.com>
2024-01-20 11:45:04 +0000

    Made the navbar work

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const navElements = document.querySelectorAll("nav #nav-container a");
+
 function menuToggle(x) {
     x.classList.toggle("menuAlt");
     document.getElementById("nav").classList.toggle("hide");
 }
+
+const navbarItems = document.querySelectorAll("nav #nav-container a");
+navbarItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        const activeItem = document.querySelector(".active");
+
+        if (activeItem) {
+            activeItem.classList.remove("active");
+        }
+
+        item.classList.add("active");
+    });
+});
