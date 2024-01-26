15dea0d1ed18a9a034929cd6bc8e60c240706cdc
TrudeEH <ehtrude@gmail.com>
2024-01-26 17:26:31 +0000

    Moved new code to root

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const navElements = document.querySelectorAll("nav #nav-container a");
-articles = document.querySelectorAll("article");
 pointer = 1;
 
 function menuToggle(x) {
@@ -18,79 +17,4 @@ function menuToggle(x) {
     }
     x.classList.toggle("menuAlt");
     document.getElementById("nav").classList.toggle("hide");
-}
-
-const navbarItems = document.querySelectorAll("nav #nav-container a");
-navbarItems.forEach((item) => {
-    item.addEventListener("click", () => {
-        const activeItem = document.querySelector(".active");
-
-        if (activeItem) {
-            activeItem.classList.remove("active");
-        }
-
-        item.classList.add("active");
-        pointer = p;
-    });
-});
-
-window.addEventListener("wheel", (event) => {
-    if (event.deltaY > 0) {
-        // Scroll down
-        scrollToNextArticle();
-    } else if (event.deltaY < 0) {
-        // Scroll up
-        scrollToPreviousArticle();
-    }
-});
-
-function scrollToNextArticle() {
-    if (pointer < articles.length) {
-        const currentArticle = articles[pointer - 1];
-        const nextArticle = articles[pointer];
-        if (nextArticle) {
-            // Calculate the offset to scroll to
-            const offset = pointer * currentArticle.clientHeight;
-            window.scrollTo({ top: offset, behavior: "smooth" });
-            pointer = pointer + 1;
-
-            // Update nav
-            const activeItem = document.querySelector(".active");
-
-            if (activeItem) {
-                activeItem.classList.remove("active");
-            }
-
-            navbarItems[pointer - 1].classList.add("active");
-        }
-    } else if (pointer == articles.length) {
-        const offset = pointer * articles[pointer - 1].clientHeight;
-        window.scrollTo({ top: offset, behavior: "smooth" });
-    }
-    console.log(pointer);
-}
-
-function scrollToPreviousArticle() {
-    if (pointer > 1) {
-        const prevArticle = articles[pointer - 1];
-        pointer = pointer - 1;
-        if (prevArticle) {
-            // Calculate the offset to scroll to
-            const offset = prevArticle.offsetTop - window.innerHeight;
-            window.scrollTo({ top: offset, behavior: "smooth" });
-
-            // Update nav
-            const activeItem = document.querySelector(".active");
-
-            if (activeItem) {
-                activeItem.classList.remove("active");
-            }
-
-            navbarItems[pointer - 1].classList.add("active");
-        }
-    } else {
-        const offset = pointer.offsetTop - window.innerHeight;
-        window.scrollTo({ top: offset, behavior: "smooth" });
-    }
-    console.log(pointer);
 }
